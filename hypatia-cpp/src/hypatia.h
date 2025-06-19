75038c7fe090c0a2a1ed526926c51be93060045c
0xpapercut <0xpapercut@gmail.com>
2025-06-19 12:53:04 +0200

    Add hypatia lib target

--- hypatia-cpp/src/hypatia.h
+++ hypatia-cpp/src/hypatia.h
@@ -1,5 +1,11 @@
 #pragma once
 
+#if defined(_WIN32)
+#define EXPORT __declspec(dllexport)
+#else
+#define EXPORT __attribute__((visibility("default")))
+#endif
+
 extern "C" {
-    int sum(int x, int y);
+EXPORT int sum(int x, int y);
 }
