60f2bc78a0aecf203d22fd969db2c10b23c7cf82
0xpapercut <0xpapercut@gmail.com>
2025-06-24 14:48:05 +0200

    Add save_csv

--- hypatia-python/utils.py
+++ hypatia-python/utils.py
@@ -1,8 +1,16 @@
+import os
 import pathlib
 import pandas as pd
 
 UNITY_STREAMING_ASSETS_DIR = pathlib.Path('../hypatia-unity/Assets/StreamingAssets')
 
-"""Read a csv file inside Unity Streaming Assets folder"""
-def read_csv(filename):
+def read_csv(filename: str):
+    """Read a csv file inside Unity Streaming Assets folder"""
     return pd.read_csv(UNITY_STREAMING_ASSETS_DIR / filename)
+
+def save_csv(df: pd.DataFrame, filename: str, overwrite: bool = False):
+    """Save a csv file inside Unity Streaming Assets folder"""
+    path = UNITY_STREAMING_ASSETS_DIR / filename
+    if os.path.exists(path) and not overwrite:
+        raise Exception(f"{path} already exists")
+    df.to_csv(UNITY_STREAMING_ASSETS_DIR / filename)
