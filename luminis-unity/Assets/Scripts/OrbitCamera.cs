9b4999ce13c5e2b3f230abc0da7e5ee13c8c25e6
0xpapercut <0xpapercut@gmail.com>
2025-06-26 18:14:16 +0200

    Update OrbitCamera

--- luminis-unity/Assets/Scripts/OrbitCamera.cs
+++ luminis-unity/Assets/Scripts/OrbitCamera.cs
@@ -9,8 +9,10 @@ public class OrbitCamera : MonoBehaviour {
     [Range(1f, 20f)]
     public float distance = 5f;
     [Range(1f, 10f)]
-    public float speed = 2f;
+    public float speed = 4f;
     public bool lockDistance = true;
+
+    private Vector3 eulerAngles;
 
     void OnValidate() {
         // Rotate camera
@@ -26,18 +28,21 @@ public class OrbitCamera : MonoBehaviour {
         }
     }
 
+    void Start() {
+        eulerAngles = transform.eulerAngles;
+    }
+
     void LateUpdate() {
         // Validate position
         OnValidate();
         // Calculate rotation
-        var angles = transform.eulerAngles;
         if (Input.GetMouseButton(0)) {
-            angles.y += Input.GetAxis("Mouse X") * speed;
-            angles.x -= Input.GetAxis("Mouse Y") * speed;
-            angles.x = Mathf.Clamp(angles.x, -89f, 89f);
+            eulerAngles.y += Input.GetAxis("Mouse X") * speed;
+            eulerAngles.x -= Input.GetAxis("Mouse Y") * speed;
+            eulerAngles.x = Mathf.Clamp(eulerAngles.x, -89f, 89f);
         }
         // Update position and rotation
-        Quaternion rotation = Quaternion.Euler(angles);
+        Quaternion rotation = Quaternion.Euler(eulerAngles);
         transform.position = target.position - rotation * Vector3.forward * distance;
         transform.rotation = rotation;
     }
